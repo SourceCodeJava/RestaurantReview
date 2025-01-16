1b3545093f7c63553ab1b5e1e436219b0eab449a
Sale <tigymthomas@gmail.com>
2025-01-16 08:02:49 +0300

    class file name change

--- src/main/java/com/sample/restaurantreview/RestaurantReviewApplication.java
+++ src/main/java/com/sample/restaurantreview/RestaurantReviewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiningApplication {
+public class RestaurantReviewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiningApplication.class, args);
+		SpringApplication.run(RestaurantReviewApplication.class, args);
 	}
 
 }
